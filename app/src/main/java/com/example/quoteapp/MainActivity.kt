254e4a234499404fdd961c7100ac1fba18c40c26
uzair hassan <uzairhassan206@gmail.com>
2023-12-20 22:45:03 +0500

    first commit

---
+++ app/src/main/java/com/example/quoteapp/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.example.quoteapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.quoteapp.ui.theme.QuoteAppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+        }
+    }
+}
